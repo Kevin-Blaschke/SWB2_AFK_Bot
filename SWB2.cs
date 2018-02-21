358fcfbc369c21950df85c017b36d9f5879fc7cb
Kevin Blaschke <kevinblaschke@utexas.edu>
2018-02-20 22:32:51 -0600

    Add files via upload

---
+++ SWB2.cs
@@ -0,0 +1,41 @@
+﻿using PS4MacroAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWB2
+{
+    public class SWB2 : ScriptBase
+    {
+        //public static Random rand = new Random();
+        /*Constructor */
+        public SWB2()
+        {
+
+            Config.Name = "SWB2";
+            Config.Scenes = new List<Scene>()
+            {
+                    new Spawn()
+            };
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+          
+          HandleScenes(); 
+          Sleep(5000);
+            //int r = rand.Next(0, 256); 
+          Press(new DualShockState() { LY = (byte)0}, 5000);
+            //Press(new DualShockState() { R2 = 0xff });
+          Press(new DualShockState() { Cross = true });
+        }
+    }
+}
---
+++ Spawn.cs
@@ -0,0 +1,34 @@
+﻿using PS4MacroAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWB2
+{
+    class Spawn : Scene
+    {
+        public override string Name => "Spawn Screen";
+
+        public static RectMap key = new RectMap()
+        {
+            X = 61,
+            Y = 538,
+            Width = 85,
+            Height = 151,
+            Hash = 18410856686823440512
+        };
+
+        public override bool Match(ScriptBase script)
+        {
+            return script.MatchTemplate(key, 50);
+        }
+
+        public override void OnMatched(ScriptBase script)
+        {
+            script.Press(new DualShockState() { Cross = true });
+        }
+    }
+}
